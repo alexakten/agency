fda7488f31f39d7ede3f0ece3c9682a26042819a
Alex <alex.akten@outlook.com>
2024-09-02 13:14:26 +0200

    analytics and inter variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-import { GeistSans } from "geist/font/sans";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "First House",
@@ -40,8 +37,10 @@ export default function RootLayout({
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/zhy0giy.css" />
       </head>
-      {/* <body className="font-neue">{children}</body> */}
-      <body className={inter.className}>{children}</body>
+      <body>
+        <Analytics />
+        {children}
+      </body>
     </html>
   );
 }
